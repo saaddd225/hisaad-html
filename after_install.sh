de4cc8666bf95202f0e91a2f9d05492e49436f1d
saaddd225 <saadchaudhryyy@gmail.com>
2025-01-16 15:39:27 +0500

    Added after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,11 +1,15 @@
 version: 0.0
 os: linux
 files:
-  - source: /index.html
-    destination: /var/www/html/index.html
-
+  - source: /   # Specify the source directory or files to deploy
+    destination: /var/www/html/   # The destination path on the EC2 instance
 hooks:
+  BeforeInstall:
+    - location: scripts/before_install.sh
+      timeout: 180
   AfterInstall:
     - location: scripts/after_install.sh
-      timeout: 300
-      runas: ec2-user
+      timeout: 180
+  ApplicationStart:
+    - location: scripts/application_start.sh
+      timeout: 180
